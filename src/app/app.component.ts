981eb02539134f9d4b901edbe154c8a19b59659e
Maria Shorina <mshorina@garderobo.ai>
2024-02-04 03:35:52 +0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
     styleUrl: './app.component.less',
 })
 export class AppComponent {
-    title = 'study-project';
+    title = 'study-angular-project';
 }
